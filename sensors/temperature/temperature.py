b8f74178e4a201a82a85be7d0bab8d934524225b
LostEmanuel <175217646+Arvidian64@users.noreply.github.com>
2026-03-25 09:44:10 +0100

    Changed sensor_types

--- sensors/temperature/temperature.py
+++ sensors/temperature/temperature.py
@@ -3,6 +3,8 @@ import datetime
 import time 
 import random
 
+from sensors.pressure.pressure import date_and_time
+
 date_and_time = datetime.datetime
 
 def temp(sensor_id, value, timestamp):
@@ -17,18 +19,17 @@ def temp(sensor_id, value, timestamp):
 
     return temp
 
-url = "http://flask-server:8080/measurements"
+url = "http://flask-server:5000/measurements"
 
 while True: 
     try:
         sensor_id = 14
         value = random.randint(-20, 50)
-        timestamp = datetime.utcnow().strftime("%Y-%m-%d | %H:%M")
+        timestamp = date_and_time.now().strftime("%Y-%m-%d | %H:%M:%S")
         data = temp(sensor_id, value, timestamp)
         response = requests.post(url, json=data)
-        time.sleep(2)
+        time.sleep(5)
 
     except Exception as e:
 
-        print("STOPPED")
-        break 
+        print("STOPPED", e)
